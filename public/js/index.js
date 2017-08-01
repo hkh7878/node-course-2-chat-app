bf18bd4b636e067bf853f6e41e8f1081507dc79e
Han Kwang Hoon <kwanghoon.han@gmail.com>
2017-08-01 10:01:20 +0900

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,6 @@ var socket = io();
 // 서버정보 수신 설정
 socket.on('connect', function () {
   console.log('Conneted to server');
-
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    test: 'Yup, that works for me'
-  });
 });
 
 socket.on('disconnect', function () {
